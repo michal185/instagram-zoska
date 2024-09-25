4fd25cfb7fd4fc7ecdadc1fe53769c110f907e24
michal185 <lovasmichal18@gmail.com>
2024-09-25 13:53:51 +0200

    initial

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import "./globals.css";
+
+
+export const metadata: Metadata = {
+  title: "SnapZoška",
+  description: "Created by Michal",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="sk">
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ src/app/not-found.tsx
@@ -0,0 +1,11 @@
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+
+export default function NotFound() {
+
+  return (
+        <Box>
+            <Typography> Nič sa tu nenachádza </Typography>
+        </Box>
+  );
+}
